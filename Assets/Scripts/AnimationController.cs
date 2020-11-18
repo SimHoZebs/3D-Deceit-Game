3025e595bb31652db3b048bf425d47991ff1827b
Sim Ho <simhoover9000@gmail.com>
2020-11-18 15:33:19 +0900

    first commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    private Animator model;
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+}
