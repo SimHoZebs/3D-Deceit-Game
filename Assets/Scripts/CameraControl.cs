0711219b82ea9ee950331c80eaec4172d9a0caf5
Sim Ho <simhoover9000@gmail.com>
2020-11-18 16:46:29 +0900

    Added comments, optimized code & organized scripts

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //customization
+    [SerializeField] private GameObject attachTarget;
+    [SerializeField] private Vector3 camOffSet = new Vector3(0f, 1.6f, 0f);
+    [SerializeField] private float camSensitivity = 2.0f;
+
+    //Visualization purpose
+    [SerializeField] private float xAxis = 0.0f;
+    [SerializeField] private float yAxis = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Have same position as the attachTarget with an offset
+        transform.position = attachTarget.transform.position + camOffSet;
+
+        //get mouse coords
+        xAxis += camSensitivity * Input.GetAxis("Mouse X");
+        yAxis -= camSensitivity * Input.GetAxis("Mouse Y");
+
+        //convert mouse coords to eulerAngles 
+        transform.eulerAngles = new Vector3(yAxis, xAxis, 0.0f);
+
+        //same I too don't know wtf Euler Angles are but it works
+        //Thanks StackOverflow
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    //Speed Control
+    [SerializeField] float walkAccel;
+    [SerializeField] float runAccel;
+    [SerializeField] float runMaxSpeed;
+    [SerializeField] float walkMaxSpeed;
+
+    //Read Input
+    private bool heldW;
+    private bool heldS;
+    private bool heldD;
+    private bool heldA;
+    private bool heldShift;
+
+    //object instancing
+    [SerializeField] private GameObject cam;
+    private Animator animator;
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        //caching gameObjs
+        animator = gameObject.GetComponent<Animator>();
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ReadInput();
+
+        float currentAccel = heldShift? runAccel : walkAccel;
+
+        //get cam's Vector3 converted from local Z & X axis to global.
+        var side = cam.transform.right;
+        var forward = cam.transform.forward;
+        //lock their Y axis to 0 because we don't wanna be moving upwards
+        forward.y = side.y = 0f;
+
+        //Basic WASD movement using vector arithmetic
+        var targetVel = new Vector3(0,0,0);
+
+        if (heldW){
+            targetVel += forward;
+        }
+        if (heldS){
+            targetVel -= forward;
+        }
+        if (heldD){
+            targetVel += side;
+        }
+        if (heldA){
+            targetVel -= side;
+        }
+
+        //Normalize vector size and multiply to target accel for constant vel
+        rigidBody.velocity = Vector3.Normalize(targetVel) * currentAccel;
+    }
+
+    private void ReadInput(){
+        heldW = Input.GetKey(KeyCode.W);
+        heldS = Input.GetKey(KeyCode.S);
+        heldD = Input.GetKey(KeyCode.D);
+        heldA = Input.GetKey(KeyCode.A);
+        heldShift = Input.GetKey(KeyCode.LeftShift);
+    }
+}
