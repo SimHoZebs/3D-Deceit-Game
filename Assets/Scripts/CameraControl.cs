83b42af40e9acfac701d07ae0ef986d89bd949ff
Sim Ho <simhoover9000@gmail.com>
2020-11-23 08:17:56 +0900

    Base building for different camera modes (#10)
    Minor changes
    - Temporary floating canvas isn't 100 times the size they should be

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    [SerializeField] private string defaultCamMode = "HeadFollowMode";
     //customization
     [SerializeField] private GameObject attachTarget;
-    [SerializeField] private Vector3 camOffSet = new Vector3(0f, 1.6f, 0f);
+    [SerializeField] private Vector3 camOffset = new Vector3(0f, 1.6f, 0f);
     [SerializeField] private float camSensitivity = 2.0f;
 
     //Visualization purpose
     [SerializeField] private float xAxis, yAxis = 0.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
+    [SerializeField] private string currentCamMode, prevCamMode;
+
+    private void Start() {
+        prevCamMode = currentCamMode = defaultCamMode;
+    }
+    private void Update(){
+
+        ChangeCamMode();
+
+        switch (currentCamMode)
+        {
+            default:
+                HeadFollowMode();
+                break;
+            case "HeadFollowMode":
+                HeadFollowMode();
+                break;
+            case "InteractionMode":
+                InteractionMode();
+                break;
+        }
+    }
+
+    private void ChangeCamMode(){
+
+        // T is the default key to change camMode. It must be held.
+        if (Input.GetKey(KeyCode.T)){
+            currentCamMode = "InteractionMode";
+        }
+        else{
+            currentCamMode = defaultCamMode;
+        }
+
+        //track previous camera mode
+        prevCamMode = currentCamMode;
+    }
+
+    public void HeadFollowMode(){
+
         //Have same position as the attachTarget with an offset
-        transform.position = attachTarget.transform.position + camOffSet;
+        transform.position = attachTarget.transform.position + camOffset;
 
         //get mouse coords
         xAxis += camSensitivity * Input.GetAxis("Mouse X");
@@ -27,5 +64,9 @@ public class CameraControl : MonoBehaviour
 
         //same I too don't know wtf Euler Angles are but it works
         //Thanks StackOverflow
+    }
+
+    public void InteractionMode(){
+
     }
 }
