aaa45e86b7de20815690d6e610ea14218e91f0c6
Sim Ho <simhoover9000@gmail.com>
2020-12-02 07:49:14 +0900

    #23 complete

--- Assets/Scripts/Tasks/InspectSampleTask.cs
+++ Assets/Scripts/Tasks/InspectSampleTask.cs
@@ -4,10 +4,71 @@ using UnityEngine;
 
 public class InspectSampleTask : TaskBase
 {
+    [Header("Cusomization")]
+    [SerializeField] private float taskDuration = 60f;
+    [SerializeField] public Color sampleDefaultColor;
+    [SerializeField] private Color correctSampleColor;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Samples")]
+    private InspectSampleTaskSample sample;
+    public List<GameObject> sampleList = new List<GameObject>();
+    public GameObject chosenSample;
+
+    public override void Start() {
+        //This Start() runs later than InspectSampleTaskSample... so far
+        base.Start();
+        sample = gameObject.GetComponentInChildren<InspectSampleTaskSample>();
+    }
+
+    public override void TaskStartRsvp(GameObject task, GameObject player)
     {
-        
+        base.TaskStartRsvp(task, player);
+
+        if (task == thisTaskObj){
+            StartCoroutine(TaskStart());
+            StopCoroutine(TaskStart());
+        }
+    }
+
+    public IEnumerator TaskStart(){
+
+        yield return new WaitForSeconds(taskDuration);
+
+        var randomIndex = Random.Range(0, sampleList.Count);
+        sampleList[randomIndex].GetComponent<InspectSampleTaskSample>().ChangeColor(correctSampleColor);
+
+        yield return new WaitUntil(() => chosenSample != null);
+
+        var chosenSampleColor = chosenSample.GetComponent<MeshRenderer>().material.color;
+
+        if (chosenSampleColor == correctSampleColor){
+            Debug.Log("Chose right sample");
+            TaskFinish(thisTaskObj);
+        }
+        else{
+            TaskStopRsvp(thisTaskObj);
+        }
+    }
+
+    public override void TaskStopRsvp(GameObject task)
+    {
+        base.TaskStopRsvp(task);
+        ClearTaskingPlayerInfo();
+    }
+
+    public override void TaskFinish(GameObject task)
+    {
+        base.TaskFinish(task);
+        ClearTaskingPlayerInfo();
+
+    }
+
+    public override void ClearTaskingPlayerInfo()
+    {
+        base.ClearTaskingPlayerInfo();
+
+        chosenSample.GetComponent<MeshRenderer>().material.color = sampleDefaultColor;
+        chosenSample = null;
+
     }
 }
---
+++ Assets/Scripts/Tasks/InspectSampleTaskSample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectSampleTaskSample : MonoBehaviour
+{
+    private InspectSampleTask inspectSampleTask;
+    private GameObject thisObj;
+    private InteractHandler interactionHandler;
+    private Material thisObjMaterial;
+
+    public void Start() {
+        //This Start() runs earlier than InspectSampleTask... so far
+
+        //Adding this as an interactable obj in each playerInteractHandler
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")){
+            var playerInteractHandler = player.GetComponentInChildren<InteractHandler>();
+            playerInteractHandler.interactableRsvps += InteractableRsvp;
+        }
+
+        thisObj = gameObject;
+        thisObjMaterial = thisObj.GetComponent<MeshRenderer>().material;
+
+        inspectSampleTask = thisObj.GetComponentInParent<InspectSampleTask>();
+        ChangeColor(inspectSampleTask.sampleDefaultColor);
+        inspectSampleTask.sampleList.Add(gameObject);
+
+    }
+
+    public void ChangeColor(Color color){
+        Debug.Log("I'm changing color!");
+        thisObjMaterial.color = color;
+    }
+
+    private void InteractableRsvp(GameObject player, GameObject interactable){
+        if (thisObj == interactable){
+
+            Debug.Log(player.name + "Interacted with this");
+            inspectSampleTask.chosenSample = thisObj;
+        }
+    }
+
+}
